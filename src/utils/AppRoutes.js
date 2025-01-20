9be9906ed0393e6e85641949cbe87567bc67d142
Titan antony <antonytitan96@gmail.com>
2025-01-20 15:56:44 +0300

    Fix: the routes for the files and install the necessary dependancies

--- src/utils/AppRoutes.js
+++ src/utils/AppRoutes.js
@@ -3,20 +3,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+
 // Components
-import HomePage from './components/HomePage';
-import NotFoundPage from './components/NotFoundPage';
+import Navbar from '../components/Shared/Navbar';
+import HomePage from '../components/HomePage';
+import NotFoundPage from '../components/NotFoundPage';
 
 const AppRoutes = () => {
   return (
     <Router>
-      <Routes>
-        {/* Primary Route for Home Page */}
-        <Route path="/" element={<HomePage />} />
+      <>
+        {/* Navigation Bar */}
+        <Navbar />
 
-        {/* Fallback Route for No Page Found */}
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+        {/* Application Routes */}
+        <Routes>
+          {/* Primary Route for Home Page */}
+          <Route path="/" element={<HomePage />} />
+
+          {/* Fallback Route for No Page Found */}
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </>
     </Router>
   );
 };
