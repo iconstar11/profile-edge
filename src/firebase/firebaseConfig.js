22dbd0d4666920bf3e59abc3c3d84f16cddc1bd8
Titan antony <antonytitan96@gmail.com>
2025-01-20 15:20:21 +0300

    Fix: git ignore the api keys for my configs

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,21 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyA_kYvODfmZ7XcnGLCBuxKHlaM8gA-20HE",
-  authDomain: "profile-edge-e5854.firebaseapp.com",
-  projectId: "profile-edge-e5854",
-  storageBucket: "profile-edge-e5854.firebasestorage.app",
-  messagingSenderId: "755712020403",
-  appId: "1:755712020403:web:b56afd4ab06eed1b0385c6",
-  measurementId: "G-7H6XPQVZWY"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+export { app, analytics };
