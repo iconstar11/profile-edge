9ed4653ba211709b2c4f708e3fc59278364879ad
Titan antony <antonytitan96@gmail.com>
2025-01-24 21:24:38 +0300

    Fix: remove the toggle theme wrapped to the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,12 @@
 import React from 'react';
 import './App.css'; // Optional: Import global CSS for your app
 import AppRoutes from './utils/AppRoutes';
-import { ThemeProvider } from './utils/ThemeContext';
+
 
 const App = () => {
   return (
-    <ThemeProvider>
+    
       <AppRoutes />
-    </ThemeProvider>
   );
 };
 
