ac0ac50193bcbc08faa1860559c0f631ce6f9d53
Titan antony <antonytitan96@gmail.com>
2025-01-24 21:32:44 +0300

    Chore: add the theme Icon on navigation and restructure my components

--- frontend/src/components/Shared/Navbar.js
+++ frontend/src/components/Shared/Navbar.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import ThemeToggle from '../../utils/ThemeToggle'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav style={{
+      backgroundColor: 'var(--bg-color)',
+      padding: '1rem',
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center'
+    }}>
+      <h1 style={{ color: 'var(--text-color)' }}>Your Logo</h1>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <ThemeToggle />
+      </div>
+    </nav>
   )
 }
 
