a9a5f34d05be53cdd2bb803bfbd430abb6d72048
Titan antony <antonytitan96@gmail.com>
2025-02-03 19:24:11 +0300

    Chore: add the necessary libraries

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,14 @@
 
 import React from 'react';
 import './App.css'; // Optional: Import global CSS for your app
-import AppRoutes from './utils/AppRoutes';
+import AppRoutes from './utils/AppRoutes'; // Import the AppRoutes component
+
+
+
 
 
 const App = () => {
+   
   return (
     
       <AppRoutes />
