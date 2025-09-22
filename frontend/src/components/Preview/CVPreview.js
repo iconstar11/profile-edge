6e56b6ad634823b9f1bbfe6d96617cada7eabf5f
Titan antony <antonytitan96@gmail.com>
2025-09-22 15:25:58 +0300

    Chore: Finish Preview tab refactor

--- frontend/src/components/Preview/CVPreview.js
+++ frontend/src/components/Preview/CVPreview.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import './CVPreview.css'
 
 function CVPreview() {
-  return (
-    <div className="preview-panel">
-          <h3>CV Preview</h3>
-          <div className="preview-box">
-            <p className="preview-name">Your Name</p>
-            <p className="preview-placeholder">[Template Preview Here]</p>
-          </div>
+    return (
+        <div className="preview-panel">
+            <h3>CV Preview</h3>
+            <div className="preview-box">
+                <p className="preview-name">Your Name</p>
+                <p className="preview-placeholder">[Template Preview Here]</p>
+            </div>
         </div>
-  )
+    )
 }
 
 export default CVPreview
