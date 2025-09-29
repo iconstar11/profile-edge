9715311f7157b125dc0dd86d23aa70fc09a46450
Titan antony <antonytitan96@gmail.com>
2025-09-29 15:25:30 +0300

    Del: the wrap of cvprovider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,12 @@
 // src/App.js
-
 import React from 'react';
-import './App.css'; // Optional: Import global CSS for your app
-import AppRoutes from './utils/AppRoutes'; // Import the AppRoutes component
-import { CVProvider } from './pages/Templates/PriviewPage/context/CVContext';
-
-
-
-
+import './App.css';
+import AppRoutes from './utils/AppRoutes';
 
 const App = () => {
-   console.log('App rendering');
+  console.log('App rendering');
   return (
-    <CVProvider>
-      <AppRoutes />
-    </CVProvider>
+    <AppRoutes />
   );
 };
 
