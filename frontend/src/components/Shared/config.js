be788a9843a2e0dc543796f6561e971780f4ccf3
iconstar11 <antonytitan96@gmail.com>
2025-10-07 12:35:07 +0300

    Fix: changethe base url to the latest version

--- frontend/src/components/Shared/config.js
+++ frontend/src/components/Shared/config.js
@@ -2,3 +2,12 @@
 export const BASE_URL = "http://127.0.0.1:5001/profileedgelatest/us-central1";
 // For production:
 // export const BASE_URL = "https://us-central1-profileedgelatest.cloudfunctions.net";
+
+/**
+ * 
+const isLocal = window.location.hostname === "localhost";
+
+const BASE_URL = isLocal
+  ? "http://127.0.0.1:5001/profileedgelatest/us-central1"
+  : "https://us-central1-profileedgelatest.cloudfunctions.net";
+ */
